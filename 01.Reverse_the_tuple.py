f207daf5a0f26d6ad621c366f14f91145bd20366
BhushanAmbilkar <ambilkarbhushan@gmail.com>
2023-05-10 14:10:43 +0530

    Tupple Exercises

---
+++ 01.Reverse_the_tuple.py
@@ -0,0 +1,9 @@
+# Exercise 1: Reverse the tuple
+
+# Show Hint
+# Use tuple slicing to reverse the given tuple.
+# Note: the last element starts at -1.
+
+tuple1 = (10, 20, 30, 40, 50)
+tuple1 = tuple1[::-1]
+print(tuple1)
---
+++ 02.Access_Value.py
@@ -0,0 +1,16 @@
+# Exercise 2: Access value 20 from the tuple
+
+# The given tuple is a nested tuple. write a Python program to print the value 20.
+
+
+# Show Hint
+# The given tuple is a nested tuple. Use indexing to locate the specified item.
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+# understand indexing
+# tuple1[0] = 'Orange'
+# tuple1[1] = [10, 20, 30]
+# list1[1][1] = 20
+
+print(tuple1[1][1])
